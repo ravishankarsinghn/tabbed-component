8ecae19a72f64081976a32c962389f151669750d
Ravi Shankar <ravishankarsinghnissan56378@gmail.com>
2023-01-05 23:29:28 +0530

    build a tab component

---
+++ script.js
@@ -0,0 +1,24 @@
+"use-strict";
+
+// Tabbed component
+const tabs = document.querySelectorAll(".operations__tab");
+const tabsContainer = document.querySelector(".operations__tab-container");
+const tabsContent = document.querySelectorAll(".operations__content");
+
+tabsContainer.addEventListener("click", function (e) {
+  const clicked = e.target.closest(".operations__tab");
+
+  // Guard clause
+  if (!clicked) return;
+
+  // remove active class
+  tabs.forEach((t) => t.classList.remove("operations__tab--active"));
+  tabsContent.forEach((c) => c.classList.remove("operations__content--active"));
+
+  // activate tab
+  clicked.classList.add("operations__tab--active");
+  // activate content area
+  document
+    .querySelector(`.operations__content--${clicked.dataset.tab}`)
+    .classList.add("operations__content--active");
+});
